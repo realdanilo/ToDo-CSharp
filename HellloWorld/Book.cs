1f42beae0ad5dc5906f3f63ca53f1423098d0cfc
Danilo <realdanilo@users.noreply.github.com>
2020-12-16 18:13:11 -0700

    Add project files.

---
+++ HellloWorld/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HellloWorld
+{
+    class Book
+    {
+        private string author;
+        private string title;
+        private int pages;
+        private DateTime date;
+        
+        public Book(string _author, string _title, int _pages)
+        {
+            author = _author;
+            title = _title;
+            pages = _pages;
+            date = DateTime.Now;
+
+        }
+        public void printInfo()
+        {
+            Console.Write($"\t{this.title} \n\t{this.author} \n\t{this.pages}");
+
+        }
+        public bool isInsane()
+        {
+            
+            return this.pages > 100;
+        }
+        public int Pages
+        {
+            get { return this.pages; }
+            set
+            {
+                if(value > 10) { this.pages = value; }
+                else
+                {
+                    Console.WriteLine("cannot update value");
+                }
+            }
+               
+            
+        }
+        public DateTime GetTime
+        {
+            get { return this.date; }
+            
+        }
+
+    }
+}
---
+++ HellloWorld/EntryPoint.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HellloWorld
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            bool runningApp = true;
+            List<Book> bookList = new List<Book>();
+            Console.WriteLine("Hello to OOP App");
+            while (runningApp)
+            {
+                Console.WriteLine("Enter Book Name");
+                string bookName = Console.ReadLine();
+                Console.WriteLine("Enter book author");
+                string bookAuthor = Console.ReadLine();
+                Console.WriteLine("Enter number of pages in book");
+                int bookPages = int.Parse(Console.ReadLine());
+                Book myBook = new Book(bookName, bookAuthor, bookPages);
+
+                //add book to list
+                bookList.Add(myBook);
+                // ask if user wants to add more books
+                Console.WriteLine("Would you like to add more books?   \n\tEnter: Y/N");
+                runningApp = Console.ReadLine().ToLower().Contains("y") ;
+
+            }
+            //for each book, print information
+            Console.WriteLine("Printing books");
+            foreach(Book book in bookList)
+            {
+                book.printInfo();
+                Console.WriteLine($"Created: {book.GetTime} \n\t***** ");
+                
+            }
+
+
+        }
+    }
+}
+
+
+
+
+
